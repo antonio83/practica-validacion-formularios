e183bbd0f83ba6e59f48d50a7f67351176c24aed
antonio <antcebollero@gmail.com>
2015-06-03 20:16:02 +0200

    mensajesok

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,65 +1,85 @@
 'use strict';
 $(document).ready(function() {
     $('#frmalta').validate({
-            rules: {
-                nombre: {
-                    required: true,
-                    remote: 'http://www.futbolistas.com/users.php'
+        rules: {
+            usuario: {
+                required: true,
+                remote: 'http://www.futbolistas.com/users.php'
 
-                },
-                apellidos: {
-                    required: true
-                },
-                telefono: {
-                    required: true
-                },
-                email: {
-                    email: true,
-                    required: true,
-                    remote: 'http://localhost/formulario-ajax/validar_email_db.php'
-                },
-                email2: {
-                    equalTo: '#email'
-                },
-                comentarios: {
-                    required: true
-                },
-                demandante: {
-                    required: true
-                },
-                nif: {
-                    required: true
-                },
-                empresa: {
-                    required: true
-                },
-                direccion: {
-                    required: true
-                },
-                cp: {
-                    required: true
-                },
-                localidad: {
-                    required: true
-                },
-                provincia: {
-                    required: true
-                },
-                pais: {
-                    required: true
-                },
-                codigoiban: {
-                    required: true
-                },
-                formadepago: {
-                    required: true
-                },
             },
-            errorLabelContainer: '#errores',
-            wrapper: 'p'
-        }
-        
+            password: {
+                required: true
+            },
+            rpassword: {
+                required: true,
+                equalTo: '#password'
+            },
+            nombre: {
+                required: true,
+                remote: 'http://www.futbolistas.com/users.php'
 
-    );
+            },
+            apellidos: {
+                required: true
+            },
+            telefono: {
+                required: true,
+                digits: true,
+                minlength: 9,
+                maxlength: 9
+            },
+            email: {
+                email: true,
+                required: true,
+                remote: 'http://localhost/formulario-ajax/validar_email_db.php'
+            },
+            email2: {
+                equalTo: '#email'
+            },
+            comentarios: {
+                required: true,
+            },
+            demandante: {
+                required: true
+            },
+            nif: {
+                required: true,
+                nifES: true
+            },
+            empresa: {
+                required: true
+            },
+            direccion: {
+                required: true
+            },
+            cp: {
+                required: true,
+                digits: true,
+                minlength: 5,
+                maxlength: 5
+            },
+            localidad: {
+                required: true
+            },
+            provincia: {
+                required: true
+            },
+            pais: {
+                required: true
+            },
+            iban: {
+                required: true,
+                iban: true
+            },
+            formadepago: {
+                required: true
+            },
+        },
+        messages: {
+                    nombre: {
+                        lettersonly: "Introduce sólo carácteres."
+                    }
+                }
+    });
 
 });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,252 @@
-console.log('\'Allo \'Allo!');
+//nif
+
+jQuery.validator.addMethod("nifES", function(value) {
+    "use strict";
+
+    value = value.toUpperCase();
+
+    // Basic format test
+    if (!value.match("((^[A-Z]{1}[0-9]{7}[A-Z0-9]{1}$|^[T]{1}[A-Z0-9]{8}$)|^[0-9]{8}[A-Z]{1}$)")) {
+        return false;
+    }
+
+    // Test NIF
+    if (/^[0-9]{8}[A-Z]{1}$/.test(value)) {
+        return ("TRWAGMYFPDXBNJZSQVHLCKE".charAt(value.substring(8, 0) % 23) === value.charAt(8));
+    }
+    // Test specials NIF (starts with K, L or M)
+    if (/^[KLM]{1}/.test(value)) {
+        return (value[8] === String.fromCharCode(64));
+    }
+
+    return false;
+
+}, "Please specify a valid NIF number.");
+
+jQuery.validator.addMethod("nowhitespace", function(value, element) {
+    return this.optional(element) || /^\S+$/i.test(value);
+}, "No white space please");
+
+
+//iban
+
+jQuery.validator.addMethod("iban", function(value, element) {
+    // some quick simple tests to prevent needless work
+    if (this.optional(element)) {
+        return true;
+    }
+
+    // remove spaces and to upper case
+    var iban = value.replace(/ /g, "").toUpperCase(),
+        ibancheckdigits = "",
+        leadingZeroes = true,
+        cRest = "",
+        cOperator = "",
+        countrycode, ibancheck, charAt, cChar, bbanpattern, bbancountrypatterns, ibanregexp, i, p;
+
+    if (!(/^([a-zA-Z0-9]{4} ){2,8}[a-zA-Z0-9]{1,4}|[a-zA-Z0-9]{12,34}$/.test(iban))) {
+        return false;
+    }
+
+    // check the country code and find the country specific format
+    countrycode = iban.substring(0, 2);
+    bbancountrypatterns = {
+        "AL": "\\d{8}[\\dA-Z]{16}",
+        "AD": "\\d{8}[\\dA-Z]{12}",
+        "AT": "\\d{16}",
+        "AZ": "[\\dA-Z]{4}\\d{20}",
+        "BE": "\\d{12}",
+        "BH": "[A-Z]{4}[\\dA-Z]{14}",
+        "BA": "\\d{16}",
+        "BR": "\\d{23}[A-Z][\\dA-Z]",
+        "BG": "[A-Z]{4}\\d{6}[\\dA-Z]{8}",
+        "CR": "\\d{17}",
+        "HR": "\\d{17}",
+        "CY": "\\d{8}[\\dA-Z]{16}",
+        "CZ": "\\d{20}",
+        "DK": "\\d{14}",
+        "DO": "[A-Z]{4}\\d{20}",
+        "EE": "\\d{16}",
+        "FO": "\\d{14}",
+        "FI": "\\d{14}",
+        "FR": "\\d{10}[\\dA-Z]{11}\\d{2}",
+        "GE": "[\\dA-Z]{2}\\d{16}",
+        "DE": "\\d{18}",
+        "GI": "[A-Z]{4}[\\dA-Z]{15}",
+        "GR": "\\d{7}[\\dA-Z]{16}",
+        "GL": "\\d{14}",
+        "GT": "[\\dA-Z]{4}[\\dA-Z]{20}",
+        "HU": "\\d{24}",
+        "IS": "\\d{22}",
+        "IE": "[\\dA-Z]{4}\\d{14}",
+        "IL": "\\d{19}",
+        "IT": "[A-Z]\\d{10}[\\dA-Z]{12}",
+        "KZ": "\\d{3}[\\dA-Z]{13}",
+        "KW": "[A-Z]{4}[\\dA-Z]{22}",
+        "LV": "[A-Z]{4}[\\dA-Z]{13}",
+        "LB": "\\d{4}[\\dA-Z]{20}",
+        "LI": "\\d{5}[\\dA-Z]{12}",
+        "LT": "\\d{16}",
+        "LU": "\\d{3}[\\dA-Z]{13}",
+        "MK": "\\d{3}[\\dA-Z]{10}\\d{2}",
+        "MT": "[A-Z]{4}\\d{5}[\\dA-Z]{18}",
+        "MR": "\\d{23}",
+        "MU": "[A-Z]{4}\\d{19}[A-Z]{3}",
+        "MC": "\\d{10}[\\dA-Z]{11}\\d{2}",
+        "MD": "[\\dA-Z]{2}\\d{18}",
+        "ME": "\\d{18}",
+        "NL": "[A-Z]{4}\\d{10}",
+        "NO": "\\d{11}",
+        "PK": "[\\dA-Z]{4}\\d{16}",
+        "PS": "[\\dA-Z]{4}\\d{21}",
+        "PL": "\\d{24}",
+        "PT": "\\d{21}",
+        "RO": "[A-Z]{4}[\\dA-Z]{16}",
+        "SM": "[A-Z]\\d{10}[\\dA-Z]{12}",
+        "SA": "\\d{2}[\\dA-Z]{18}",
+        "RS": "\\d{18}",
+        "SK": "\\d{20}",
+        "SI": "\\d{15}",
+        "ES": "\\d{20}",
+        "SE": "\\d{20}",
+        "CH": "\\d{5}[\\dA-Z]{12}",
+        "TN": "\\d{20}",
+        "TR": "\\d{5}[\\dA-Z]{17}",
+        "AE": "\\d{3}\\d{16}",
+        "GB": "[A-Z]{4}\\d{14}",
+        "VG": "[\\dA-Z]{4}\\d{16}"
+    };
+
+    bbanpattern = bbancountrypatterns[countrycode];
+    // As new countries will start using IBAN in the
+    // future, we only check if the countrycode is known.
+    // This prevents false negatives, while almost all
+    // false positives introduced by this, will be caught
+    // by the checksum validation below anyway.
+    // Strict checking should return FALSE for unknown
+    // countries.
+    if (typeof bbanpattern !== "undefined") {
+        ibanregexp = new RegExp("^[A-Z]{2}\\d{2}" + bbanpattern + "$", "");
+        if (!(ibanregexp.test(iban))) {
+            return false; // invalid country specific format
+        }
+    }
+
+    // now check the checksum, first convert to digits
+    ibancheck = iban.substring(4, iban.length) + iban.substring(0, 4);
+    for (i = 0; i < ibancheck.length; i++) {
+        charAt = ibancheck.charAt(i);
+        if (charAt !== "0") {
+            leadingZeroes = false;
+        }
+        if (!leadingZeroes) {
+            ibancheckdigits += "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(charAt);
+        }
+    }
+
+    // calculate the result of: ibancheckdigits % 97
+    for (p = 0; p < ibancheckdigits.length; p++) {
+        cChar = ibancheckdigits.charAt(p);
+        cOperator = "" + cRest + "" + cChar;
+        cRest = cOperator % 97;
+    }
+    return cRest === 1;
+}, "Please specify a valid IBAN");
+
+
+//cif
+
+jQuery.validator.addMethod('cif', function(value, element) {
+    value = value.replace(/[\s-]/g, ""); // quitar espacios o guiones
+    par = 0;
+    non = 0;
+    letras = "ABCDEFGHKLMNPQS";
+    let = value.charAt(0);
+    if (value.length == 0) {
+        return true;
+    }
+    if (value.length != 9) {
+        //alert('El Cif debe tener 9 dígitos');
+        return false;
+    }
+    if (letras.indexOf(let.toUpperCase()) == -1) {
+        //alert("El comienzo del Cif no es válido");
+        return false;
+    }
+    for (zz = 2; zz < 8; zz += 2) {
+        par = par + parseInt(value.charAt(zz));
+    }
+    for (zz = 1; zz < 9; zz += 2) {
+        nn = 2 * parseInt(value.charAt(zz));
+        if (nn > 9) nn = 1 + (nn - 10);
+        non = non + nn;
+    }
+    parcial = par + non;
+    control = (10 - (parcial % 10));
+    if (control == 10) {
+        control = 0;
+    }
+    if (control != value.charAt(8)) {
+        //alert("El Cif no es válido");
+        return false;
+    }
+    //alert("El Cif es válido");
+    return true;
+}, "CIF incorrecto");
+
+
+//email2
+
+
+jQuery.validator.addMethod("email2", function(value, element, param) {
+    return this.optional(element) || /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)*(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i.test(value);
+}, jQuery.validator.messages.email);
+
+
+//cuenta bancaria
+
+
+jQuery.validator.addMethod('validacuentabanco', function(value, element, param) {
+	var banco = value.substring(0,4);
+    var sucursal = value.substring(4,8);
+    var dc = value.substring(8,10);
+    var cuenta=value.substring(10,20);
+    var CCC = banco+sucursal+dc+cuenta;
+    var i;
+    if (!/^[0-9]{20}$/.test(CCC)){
+        return false;
+    }
+    else
+    {
+        valores = new Array(1, 2, 4, 8, 5, 10, 9, 7, 3, 6);
+        control = 0;
+        for (i=0; i<=9; i++)
+        control += parseInt(cuenta.charAt(i)) * valores[i];
+        control = 11 - (control % 11);
+        if (control == 11) control = 0;
+        else if (control == 10) control = 1;
+        if(control!=parseInt(dc.charAt(1))) {
+            return false;
+        }
+        control=0;
+        var zbs="00"+banco+sucursal;
+        for (i=0; i<=9; i++)
+            control += parseInt(zbs.charAt(i)) * valores[i];
+        control = 11 - (control % 11);
+        if (control == 11) control = 0;
+            else if (control == 10) control = 1;
+        if(control!=parseInt(dc.charAt(0))) {
+            return false;
+        }
+        return true;
+    }
+}, 'Introduce un número de cuenta válido.');
+
+//cp
+
+// Si el Código Postal se compone de 4 dígitos, se agrega un 0 a la izquierda.
+            $("#addr_041").focusout(function() {
+                var caracteres = $("#addr_041").val();
+                if (caracteres.length == 4)
+                    $("#addr_041").val("0" + caracteres);
+            });
